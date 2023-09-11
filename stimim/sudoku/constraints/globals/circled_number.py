d55f1538f3c99e14ed7b720ab5da896a16dbcb31
Stimim Chen <stimim@chromium.org>
2023-09-11 01:54:12 +0000

    circled_number: use slots and rename local vars
    1. use __slots__ to allocate member variables
    2. don't name local vars as `board`

--- stimim/sudoku/constraints/globals/circled_number.py
+++ stimim/sudoku/constraints/globals/circled_number.py
@@ -10,6 +10,13 @@ class CircledNumberConstraintViolation(Exception):
 
 
 class CircledNumberConstraint:
+  __slots__ = [
+    'positions',
+    'allowed_digits',
+    'in_circle_count',
+    'out_circle_count',
+  ]
+
   def __init__(self, positions: set[position.Index], digits: set[int]):
     self.positions = positions
     self.allowed_digits = digits
@@ -25,16 +32,31 @@ class CircledNumberConstraint:
       digit: 0 for digit in digits
     }
 
-  def eliminate_by_given_digit(self, board: board.SudokuBoard,
+  def initialize(self, b: board.SudokuBoard):
+    b = b.copy()
+
+    for index in self.positions:
+      for digit in constants.ALL_DIGITS - self.allowed_digits:
+        b.cells[index] = b.cells[index].remove_candidate(digit)
+
+    if 9 in self.allowed_digits:
+      for index in range(len(b.cells)):
+        if index in self.positions:
+          continue
+        b.cells[index] = b.cells[index].remove_candidate(9)
+
+    return b
+
+  def eliminate_by_given_digit(self, b: board.SudokuBoard,
                                coord: position.Coord, digit: int):
     retval = self.copy()
     if coord.to_index() in self.positions:
-      board = retval._eliminate_in_circle_digit(board, digit)
+      b = retval._eliminate_in_circle_digit(b, digit)
     else:
-      board = retval._eliminate_out_circle_digit(board, digit)
-    return board, retval
+      b = retval._eliminate_out_circle_digit(b, digit)
+    return b, retval
 
-  def _eliminate_in_circle_digit(self, board, digit):
+  def _eliminate_in_circle_digit(self, b, digit):
     if digit not in self.allowed_digits:
       raise CircledNumberConstraintViolation(
           f'{digit} cannot appear in the circled cells')
@@ -44,20 +66,20 @@ class CircledNumberConstraint:
           f'{digit} cannot appear in the circled cells')
     self.in_circle_count[digit] += 1
     if self.in_circle_count[digit] < digit:
-      return board
+      return b
 
     logging.info('We have found all %d that are in circled cells. '
                  'Removing all %d in other circles', digit, digit)
-    cells = [cell for cell in board.cells]
+    cells = [cell for cell in b.cells]
     for index in self.positions:
       if cells[index].digit is None:
         cells[index] = cells[index].remove_candidate(digit)
-    return board.__class__(cells)
+    return b.__class__(cells)
 
-  def _eliminate_out_circle_digit(self, board, digit):
+  def _eliminate_out_circle_digit(self, b, digit):
     if digit not in self.allowed_digits:
       # We don't care.
-      return board
+      return b
     if self.out_circle_count[digit] + digit == constants.BOARD_SIZE:
       # this should not happen, but worth checking
       raise CircledNumberConstraintViolation(
@@ -65,17 +87,17 @@ class CircledNumberConstraint:
 
     self.out_circle_count[digit] += 1
     if self.out_circle_count[digit] + digit != constants.BOARD_SIZE:
-      return board
+      return b
 
     logging.info('We have found all %d that are not in circled cells. '
                  'Removing all %d in other non-circled cells', digit, digit)
-    cells = [cell for cell in board.cells]
+    cells = [cell for cell in b.cells]
     for index in range(constants.BOARD_SIZE * constants.BOARD_SIZE):
       if index in self.positions:
         continue
       if cells[index].digit is None:
         cells[index] = cells[index].remove_candidate(digit)
-    return board.__class__(cells)
+    return b.__class__(cells)
 
   def copy(self):
     retval = self.__class__(self.positions, self.allowed_digits)
